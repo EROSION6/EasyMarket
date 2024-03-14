c3ae20021a9cd7902647bc7e3842abf89f6a1ff3
EROSION6 <147639244+EROSION6@users.noreply.github.com>
2024-03-14 14:02:30 +0300

    Change adding items to cart

--- src/Context/Cart.jsx
+++ src/Context/Cart.jsx
@@ -6,16 +6,32 @@ export const AppContext = ({ children }) => {
 	const [carts, setCarts] = useState([])
 
 	const handleAddCard = ({ id, title, imageUrl, price, count }) => {
-		setCarts([
-			...carts,
-			{
-				id,
-				title,
-				imageUrl,
-				price,
-				count,
-			},
-		])
+		const find = carts.find(item => item.id === id)
+		if (find) {
+			setCarts(
+				carts.map(item => {
+					if (item.id === id) {
+						return {
+							...item,
+							count: item.count + 1,
+						}
+					} else {
+						return item
+					}
+				})
+			)
+		} else {
+			setCarts([
+				...carts,
+				{
+					id,
+					title,
+					imageUrl,
+					price,
+					count,
+				},
+			])
+		}
 	}
 
 	const handleDeleteCard = id => {
